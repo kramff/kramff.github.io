672edb5beb3b42fe9a267de2e769fb839489b518
kramff <kramff@gmail.com>
2015-08-16 10:51:13 -0400

    Made iPad js

---
+++ ipad.js
@@ -0,0 +1,10 @@
+
+element.addEventListener("touchstart", touchStart, false);
+//element.addEventListener("touchmove", touchMove, false);
+//element.addEventListener("touchend", touchEnd, false);
+//element.addEventListener("touchcancel", touchCancel, false);
+
+function touchStart () {
+player.y -= 1;
+}
+
